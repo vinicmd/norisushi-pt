9bbadcd70fd5b7069071366e9e09a2ed9bbc5796
Vinicius Duarte <viniciusduarte@outlook.com>
2023-07-27 14:53:13 +0100

    chore: add axios and loading component

--- src/app/buffet/[_id]/page.tsx
+++ src/app/buffet/[_id]/page.tsx
@@ -5,6 +5,8 @@ import {useRouter} from 'next/navigation'
 
 import {api} from '@/services/api'
 import styles from '@/app/buffet/[_id]/styles.module.css'
+import {Loading} from '@/components/Loading'
+import {AxiosError} from 'axios'
 
 export default function Page({params}: {params: {_id: string}}) {
   const [isLoading, setIsLoading] = useState(true)
@@ -36,15 +38,20 @@ export default function Page({params}: {params: {_id: string}}) {
       try {
         const response = await api.get(`/orders/${params._id}`)
         const order = response.data[0] as Order
+        if (order.status !== 'OPEN') return router.push('/')
         setOrder(order)
-        if (order.status !== 'OPEN') return router.replace('/')
       } catch (error: unknown) {
-        console.log(error)
+        const {response} = error as AxiosError
+        if (response?.status === 404) return router.push('/')
       } finally {
         setIsLoading(false)
       }
     }
     generateStaticParams()
-  }, [params._id, router])
-  return !isLoading && <div className={styles.buffet}>My Post: {order._id}</div>
+  }, [order.status, params._id, router])
+  return isLoading ? (
+    <Loading />
+  ) : (
+    <div className={styles.buffet}>My Post: {order._id}</div>
+  )
 }
